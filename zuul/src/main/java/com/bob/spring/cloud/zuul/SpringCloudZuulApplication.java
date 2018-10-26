981a40d4912abb199af2b70f516641f6bf204a6b
wb-jjb318191 <wb-jjb318191@alibaba-inc.com>
2018-10-26 16:53:18 +0800

    添加Zuul(网关路由)模块

--- zuul/src/main/java/com/bob/spring/cloud/zuul/SpringCloudZuulApplication.java
+++ zuul/src/main/java/com/bob/spring/cloud/zuul/SpringCloudZuulApplication.java
@@ -3,11 +3,16 @@ package com.bob.spring.cloud.zuul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.ZuulProxyAutoConfiguration;
+import org.springframework.cloud.netflix.zuul.ZuulServerAutoConfiguration;
 
 /**
+ * SPI机制引入两个配置类: {@link ZuulServerAutoConfiguration}, {@link ZuulProxyAutoConfiguration}
+ *
  * @author wb-jjb318191
  * @create 2018-10-26 14:04
  */
+//@EnableZuulServer
 @EnableZuulProxy
 @SpringBootApplication
 public class SpringCloudZuulApplication {
