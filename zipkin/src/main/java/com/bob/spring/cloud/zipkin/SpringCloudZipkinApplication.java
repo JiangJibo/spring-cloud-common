ab0584fb788653559e6a6cdbc51980be0ac76d69
wb-jjb318191 <wb-jjb318191@alibaba-inc.com>
2018-10-30 17:01:38 +0800

    zipkin完善

---
+++ zipkin/src/main/java/com/bob/spring/cloud/zipkin/SpringCloudZipkinApplication.java
@@ -0,0 +1,24 @@
+package com.bob.spring.cloud.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin2.server.internal.EnableZipkinServer;
+import zipkin2.server.internal.ZipkinQueryApiV2;
+
+/**
+ * {@link ZipkinQueryApiV2}
+ *
+ * @author wb-jjb318191
+ * @create 2018-10-30 14:36
+ */
+@EnableEurekaClient
+@EnableZipkinServer
+@SpringBootApplication
+public class SpringCloudZipkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudZipkinApplication.class, new String[] {"--spring.profiles.active=zipkin-http"});
+    }
+
+}
